bf96785dc3bfe405ea80adc045163341d7d659c1
Binh Tran <binhtran@kms-technology.com>
2018-01-24 15:45:41 +0700

    organize Redshift supports data type

--- ODBC/Izenda.Synergy.DataAdaptor.RDBMS.ODBC.Redshift/Constants/RedshiftSupportDataType.cs
+++ ODBC/Izenda.Synergy.DataAdaptor.RDBMS.ODBC.Redshift/Constants/RedshiftSupportDataType.cs
@@ -44,40 +44,77 @@ namespace Izenda.BI.DataAdaptor.RDBMS.ODBCRedshift.Constants
         /// </summary>
         public RedshiftSupportDataType()
         {
-            // DateTime
-            AddDatabaseDataType("DATE", IzendaDataType.DatetimeType, true, "System.DateTime");
-            AddDatabaseDataType("TIMESTAMP WITHOUT TIME ZONE", IzendaDataType.DatetimeType, true, "System.DateTime");
-            AddDatabaseDataType("TIMESTAMP WITH TIME ZONE", IzendaDataType.DatetimeType, true, "System.DateTime");
+            // Not supported: box, circle, line, lseg, path, point, polygon, tsquery, tsvector, txid_snapshot
 
-            // Numeric
-            AddDatabaseDataType("SMALLINT", IzendaDataType.NumericType, true, "System.Int16");
-            AddDatabaseDataType("INT2", IzendaDataType.NumericType, true, "System.Int16");
-            AddDatabaseDataType("INTEGER", IzendaDataType.NumericType, true, "System.Int32");
-            AddDatabaseDataType("INT", IzendaDataType.NumericType, true, "System.Int32");
-            AddDatabaseDataType("INT4", IzendaDataType.NumericType, true, "System.Int32");
-            AddDatabaseDataType("BIGINT", IzendaDataType.NumericType, true, "System.Int64");
-            AddDatabaseDataType("INT8", IzendaDataType.NumericType, true, "System.Int64");
-            AddDatabaseDataType("REAL", IzendaDataType.NumericType, true, "System.Single");
-            AddDatabaseDataType("FLOAT4", IzendaDataType.NumericType, true, "System.Single");
-            AddDatabaseDataType("DOUBLE PRECISION", IzendaDataType.NumericType, true, "System.Double");
-            AddDatabaseDataType("FLOAT", IzendaDataType.NumericType, true, "System.Double");
-            AddDatabaseDataType("FLOAT8", IzendaDataType.NumericType, true, "System.Double");
-            AddDatabaseDataType("DECIMAL", IzendaDataType.NumericType, true, "System.Decimal");
-            AddDatabaseDataType("NUMERIC", IzendaDataType.NumericType, true, "System.Decimal");
+            RegisterNumeric();
+            RegisterDateTime();
+            RegisterBoolean();
+            RegisterText();
+        }
 
-            // Boolean
-            AddDatabaseDataType("BOOLEAN", IzendaDataType.BooleanType, true, "System.Boolean", true /* default mapping */);
-            AddDatabaseDataType("BOOL", IzendaDataType.BooleanType, true, "System.Boolean" /* default mapping */);
+        private void RegisterNumeric()
+        {
+            AddDatabaseDataType("numeric", IzendaDataType.NumericType, true, "System.Decimal", true /*default mapping*/);
 
-            //// Text
-            AddDatabaseDataType("CHAR", IzendaDataType.TextType, true, "System.String");
-            AddDatabaseDataType("CHARACTER", IzendaDataType.TextType, true, "System.String");
-            AddDatabaseDataType("NCHAR", IzendaDataType.TextType, true, "System.String");
-            AddDatabaseDataType("BPCHAR", IzendaDataType.TextType, true, "System.String");
-            AddDatabaseDataType("VARCHAR", IzendaDataType.TextType, true, "System.String");
-            AddDatabaseDataType("CHARACTER VARYING", IzendaDataType.TextType, true, "System.String");
-            AddDatabaseDataType("NVARCHAR", IzendaDataType.TextType, true, "System.String");
-            AddDatabaseDataType("TEXT", IzendaDataType.TextType, true, "System.String");
+            // integer
+            AddDatabaseDataType("smallint", IzendaDataType.NumericType, true, "System.Int16");
+            AddDatabaseDataType("int2", IzendaDataType.NumericType, true, "System.Int16");
+
+            AddDatabaseDataType("integer", IzendaDataType.NumericType, true, "System.Int32");
+            AddDatabaseDataType("int", IzendaDataType.NumericType, true, "System.Int32");
+            AddDatabaseDataType("int4", IzendaDataType.NumericType, true, "System.Int32");
+
+            AddDatabaseDataType("int8", IzendaDataType.NumericType, true, "System.Int64");
+            AddDatabaseDataType("bigint", IzendaDataType.NumericType, true, "System.Int64");
+
+            // floating point number
+            AddDatabaseDataType("real", IzendaDataType.NumericType, true, "System.Single");
+            AddDatabaseDataType("float4", IzendaDataType.NumericType, true, "System.Single");
+
+            AddDatabaseDataType("float", IzendaDataType.NumericType, true, "System.Double");
+            AddDatabaseDataType("float8", IzendaDataType.NumericType, true, "System.Double");
+            AddDatabaseDataType("double precision", IzendaDataType.NumericType, true, "System.Double");
+
+            AddDatabaseDataType("decimal", IzendaDataType.NumericType, true, "System.Decimal");
+        }
+
+        private void RegisterDateTime()
+        {
+            AddDatabaseDataType("date", IzendaDataType.DatetimeType, true, "System.DateTime", true/*default mapping*/);
+
+            // timestamp is alias for [timestamp with time zone]
+            AddDatabaseDataType("timestamp", IzendaDataType.DatetimeType, true, "System.DateTime");
+            AddDatabaseDataType("timestamp with time zone", IzendaDataType.DatetimeType, true, "System.DateTime");
+
+            // timestamptz is alias for [timestamp without time zone]
+            AddDatabaseDataType("timestamptz", IzendaDataType.DatetimeType, true, "System.DateTime");
+            AddDatabaseDataType("timestamp without time zone", IzendaDataType.DatetimeType, true, "System.DateTime");
+        }
+
+        private void RegisterBoolean()
+        {
+            // bool is alias for boolean
+            AddDatabaseDataType("bool", IzendaDataType.BooleanType, true, "System.Boolean");
+            AddDatabaseDataType("boolean", IzendaDataType.BooleanType, true, "System.Boolean", true/*default mapping*/);
+        }
+
+        private void RegisterText()
+        {
+            // 4k range
+            AddDatabaseDataType("char", IzendaDataType.TextType, true, "System.String");
+            AddDatabaseDataType("character", IzendaDataType.TextType, true, "System.String");
+            AddDatabaseDataType("nchar", IzendaDataType.TextType, true, "System.String");
+            AddDatabaseDataType("national character", IzendaDataType.TextType, true, "System.String");
+
+            // 64k range
+            AddDatabaseDataType("varchar", IzendaDataType.TextType, true, "System.String", true/*default mapping*/);
+            AddDatabaseDataType("character varying", IzendaDataType.TextType, true, "System.String");
+            AddDatabaseDataType("nvarchar", IzendaDataType.TextType, true, "System.String");
+            AddDatabaseDataType("national character varying", IzendaDataType.TextType, true, "System.String");
+
+            // 265 bytes
+            AddDatabaseDataType("bpchar", IzendaDataType.TextType, true, "System.String");
+            AddDatabaseDataType("text", IzendaDataType.TextType, true, "System.String");
         }
     }
 }
